49d41a691d730a89e796949ccc375d3deb96b901
peterson <peterson_@live.com>
2019-05-29 00:08:36 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('app.channel.shared.data');
+  String dataShared = "No data";
+  String generatedPdfFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedText();
+
+
+  }
+
+
+  Future<void> generateExampleDocument() async {
+    var htmlContent = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        th, td, p {
+          padding: 5px;
+          text-align: left;
+        }
+        </style>
+      </head>
+      <body>
+        <h2>PDF Generated with flutter_html_to_pdf plugin</h2>
+
+        <table style="width:100%">
+          <caption>Sample HTML Table</caption>
+          <tr>
+            <th>Month</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>January</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>50</td>
+          </tr>
+        </table>
+
+        <p>Image loaded from web</p>
+        <img src="https://i.imgur.com/wxaJsXF.png" alt="web-img">
+      </body>
+    </html>
+    """;
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    var targetPath = appDocDir.path;
+    var targetFileName = "example-pdf";
+
+    var generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlContent, targetPath, targetFileName);
+    dataShared = generatedPdfFile.path;
+
+    final Email email = Email(
+      body: 'Email body',
+      subject: 'Email subject',
+      recipients: ['peterson_@live.com'],
+      attachmentPath: dataShared,
+    );
+
+    try {
+      await FlutterEmailSender.send(email);
+      dataShared = 'success';
+    } catch (error) {
+      dataShared = error.toString();
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text(dataShared)));
+  }
+
+  getSharedText() async {
+    var sharedData = await platform.invokeMethod("getSharedText");
+    if (sharedData != null) {
+      await generateExampleDocument();
+      setState(() {
+        dataShared = sharedData;
+      });
+      //generateExampleDocument();
+    }
+  }
+}
